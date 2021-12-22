11f4a6778129ce9f6f818f0831d2178e21f5fa51
NavaLeib <navaleib.research@gmail.com>
2021-12-22 14:07:55 -0500

    adding the script to run and plot

--- functions.py
+++ functions.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import normalize
 
 import numpy as np
 from difflib import SequenceMatcher
 import networkx as nx
+import random
+import math
+import scipy.special
+
+import pandas as pd
+
+import itertools
+
+import matplotlib.pyplot as plt
 
 
 
@@ -12,16 +24,15 @@ class SmallSystem:
     def __init__(self, barcodes, cells):
         self.barcodes = barcodes
         self.cells = cells
-        self.X=np.empty(cells, dtype="<U100")
+        self.X = np.empty(cells, dtype="<U100")
 
-
-    def generate_barcoded_cells(self,p):
+    def generate_barcoded_cells(self, p):
         XX = np.empty(self.cells, dtype="<U100")  # U6: 6 differnt letters can be inserted
         for bar in range(65, 65 + int(self.barcodes)):  # just to get 1st barcodes as 'A' (=65 in Ascii)
             for i in range(self.cells):
-                if np.random.rand() <= p:#*(len(X[i])/50)**(1/3)+0.01:
+                if np.random.rand() <= p:  # *(len(X[i])/50)**(1/3)+0.01:
                     XX[i] = XX[i] + chr(bar)
-        self.X=XX
+        self.X = XX
         return self.X
 
     def number_of_lineages(X, cells, number_of_infected):
@@ -52,13 +63,13 @@ class SmallSystem:
                     lineage = lineage + 1 / 2
         return Prob_SubSet
 
-    def overlap(X1,X2,cells):
+    def overlap(X1, X2, cells):
         Prob_overlap = 0
         for i in range(cells):
             for j in range(cells):
-                if  (len(X1[i]) > 0) & (len(X2[j]) > 0) :
-      #          if (X1[i] in X2[j]) or (X2[j] in X1[i]) :
-                    if (X1[i] == X2[j])  :
+                if (len(X1[i]) > 0) & (len(X2[j]) > 0):
+                    #          if (X1[i] in X2[j]) or (X2[j] in X1[i]) :
+                    if (X1[i] == X2[j]):
                         Prob_overlap = Prob_overlap + 1
         return Prob_overlap
 
@@ -75,78 +86,109 @@ class SmallSystem:
             Init_Barcode_Len = len(One_Cell)
             for j in range(Init_Barcode_Len - 1, -1, -1):
                 if np.random.rand() <= dropout_prob:
-                #                print(i, j, One_Cell)
+                    #                print(i, j, One_Cell)
                     One_Cell = One_Cell.replace(One_Cell[j], '')
-                #                print(i, j, One_Cell)
+                    #                print(i, j, One_Cell)
                     X_afterdrop[i] = One_Cell
         return X_afterdrop
-        
-     
 
     def SimilarityMatrix(X):
-        A=np.zeros([len(X),len(X)])
+        A = np.zeros([len(X), len(X)])
         for i in range(len(X)):
             for j in range(len(X)):
-                A[i,j]=SequenceMatcher(None,X[i],X[j]).ratio()
+                A[i, j] = SequenceMatcher(None, X[i], X[j]).ratio()
         return A
-    
-        
-def NetworkPlot(A,labels,edge_width=0.1,node_size=10):
+
+def NetworkPlot(A, labels, edge_width=0.1, node_size=10):
     dt = [('len', float)]
     A = A.view(dt)
-    G = nx.from_numpy_matrix(A,create_using=nx.MultiGraph)
-    G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),labels)))    
-    pos=nx.spring_layout(G)
+    G = nx.from_numpy_matrix(A, create_using=nx.MultiGraph)
+    G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())), labels)))
+    pos = nx.spring_layout(G)
     d = dict(G.degree)
-    nx.draw_networkx(G,pos=pos,width=edge_width,node_size=[v * node_size for v in d.values()],node_color=[(v-2)**0.5  for v in d.values()])
+    nx.draw_networkx(G, pos=pos, width=edge_width, node_size=[v * node_size for v in d.values()],
+                     node_color=[(v - 2) ** 0.5 for v in d.values()])
+    return True
 
-def sim(list1,list2):
-    if float(len(set(list1) | set(list2)))>0:
+
+def sim(list1, list2):
+    if float(len(set(list1) | set(list2))) > 0:
         res = len(set(list1) & set(list2)) / float(len(set(list1) | set(list2)))
     else:
-        res=1
+        res = 1
     return float(res)
+
+
+'''
+def BarcodesSet(X):
+  X_set={}
+  cells=X.shape[0]
+  for i in range(cells):
+    [l for l in np.flatnonzero(X[i,:])
+'''
+
 
 class LargeSystem:
 
     def __init__(self, barcodes, cells):
         self.barcodes = barcodes
         self.cells = cells
-        self.X = np.zeros([cells, barcodes], dtype=int)  
-
+        self.X = np.zeros([cells, barcodes], dtype=int)
 
     def generate_barcoded_cells(self, p, Poisson):
-        XX = np.zeros([cells, barcodes], dtype=int)  
-        if Poisson==True:
-            for bar in range(barcodes): 
+        cells=self.cells
+        barcodes=self.barcodes
+        XX = np.zeros([cells, barcodes], dtype=int)
+        if Poisson == True:
+            for bar in range(barcodes):
                 for i in range(cells):
-                    if np.random.rand() <=p:
+                    if np.random.rand() <= p:
                         XX[i][bar] = 1
         else:
-            for bar in range(barcodes): 
-                for i in range(cells):  
-                    if np.random.rand() <=(XX[i,:].sum()/barcodes)**(1/1)+1/(2*barcodes):
+            for bar in range(barcodes):
+                for i in range(cells):
+                    if np.random.rand() <= (XX[i, :].sum() / barcodes) ** (1 / 1) + 1 / (2 * barcodes):
                         XX[i][bar] = 1
-        self.X=XX
+        self.X = XX
         return self.X
 
     def dropouts(X, p):
-        barcodes=X.shape[1]
-        cells=X.shape[0]
-        for bar in range(barcodes): 
+        barcodes = X.shape[1]
+        cells = X.shape[0]
+        #    print(barcodes, cells)
+        for bar in range(barcodes):
             for i in range(cells):
-                if np.random.rand() <=p:   #(X[i,:].sum()/barcodes)**(1/1)+1/(2*barcodes):
-                    X[i][bar] = 0
+                if X[i][bar] == 1:
+                    if np.random.rand() <= p:  # (X[i,:].sum()/barcodes)**(1/1)+1/(2*barcodes):
+                        X[i][bar] = 0
         return X
 
+    def Rapid_SimilarityMatrix(X):
+        cells = X.shape[0]
+        A = np.identity(cells)
+        CellsIndexSetS = set(np.arange(cells))
+        for CellsPair in itertools.combinations(CellsIndexSetS, r=2):
+            i = CellsPair[0]
+            j = CellsPair[1]
+            A[i, j] = sim(([l for l in np.flatnonzero(X[j, :])]), ([l for l in np.flatnonzero(X[i, :])]))
+            A[j, i] = A[i, j]
+        return 1 - A
+
     def SimilarityMatrix(X):
-        A=np.zeros([len(X),len(X)])
+        A = np.zeros([len(X), len(X)])
         for i in range(len(X)):
             for j in range(len(X)):
-                A[i,j]=sim(([l for l in np.flatnonzero(X[j,:])]),([l for l in np.flatnonzero(X[i,:])]))
+                A[i, j] = sim(([l for l in np.flatnonzero(X[j, :])]), ([l for l in np.flatnonzero(X[i, :])]))
+        return 1 - A
+
+    def NumBarcodesDiff(X):
+        A = np.zeros([len(X), len(X)])
+        for i in range(len(X)):
+            for j in range(len(X)):
+                A[i, j] = sum(abs(X[j, :] - X[i, :]))
         return A
 
-    def DistanceMatrix_dropout_conditional(X, dropout_prob, p_i):
+    def DistanceMatrix_dropout(X, dropout_prob):
         A = np.zeros([len(X), len(X)])
         for i in range(len(X)):
             for j in range(i + 1):
@@ -159,15 +201,91 @@ class LargeSystem:
                     (set([l for l in np.flatnonzero(X[i, :])]) | set([l for l in np.flatnonzero(X[j, :])])) & set(
                         [l for l in np.flatnonzero(X[j, :])]))
                 # A[i,j]=0.5*((1-dropout_prob)**Pij_sim_len*dropout_prob**(Union_len-Pij_sim_len)+(1-dropout_prob)**Pji_sim_len*dropout_prob**(Union_len-Pji_sim_len))
-                A[i, j] = ((1 - dropout_prob) ** Pij_sim_len * dropout_prob ** (Union_len - Pij_sim_len) * (
-                            1 - dropout_prob) ** Pji_sim_len * dropout_prob ** (Union_len - Pji_sim_len))
-                S1 = len(set([l for l in np.flatnonzero(X[i, :])]))
-                S2 = len(set([l for l in np.flatnonzero(X[j, :])]))
-                B = (p_i) ** Overlap_len * (1 - p_i) ** (len(X) - Overlap_len) * (
-                            scipy.special.binom(100, S1) * scipy.special.binom(len(X), S2) / (
-                        scipy.special.binom(100, Union_len)))
+                A[i, j] = (((1 - dropout_prob) ** Pij_sim_len) * (dropout_prob ** (Union_len - Pij_sim_len)) * (
+                            (1 - dropout_prob) ** Pji_sim_len) * (dropout_prob ** (Union_len - Pji_sim_len)))
                 A[j, i] = A[i, j]
                 if j == i:
                     A[i, j] = 1
-        return (A / B)
+        return (1 - A)
+
+    def DistanceMatrix_dropout_conditional(X, dropout_prob, p_i):
+        A = np.zeros([len(X), len(X)])
+        cells = X.shape[0]
+        barcodes = X.shape[1]
+        CellsIndexSetS = set(np.arange(cells))
+        for CellsPair in itertools.combinations(CellsIndexSetS, r=2):
+            i = CellsPair[0]
+            j = CellsPair[1]
+            # for i in range(len(X)):
+            #    for j in range(i+1):
+            Union_len = len(set([l for l in np.flatnonzero(X[i, :])]) | set([l for l in np.flatnonzero(X[j, :])]))
+            Overlap_len = len(set([l for l in np.flatnonzero(X[i, :])]) & set([l for l in np.flatnonzero(X[j, :])]))
+            Pij_sim_len = len(
+                (set([l for l in np.flatnonzero(X[i, :])]) | set([l for l in np.flatnonzero(X[j, :])])) & set(
+                    [l for l in np.flatnonzero(X[i, :])]))
+            Pji_sim_len = len(
+                (set([l for l in np.flatnonzero(X[i, :])]) | set([l for l in np.flatnonzero(X[j, :])])) & set(
+                    [l for l in np.flatnonzero(X[j, :])]))
+            # A[i,j]=0.5*((1-dropout_prob)**Pij_sim_len*dropout_prob**(Union_len-Pij_sim_len)+(1-dropout_prob)**Pji_sim_len*dropout_prob**(Union_len-Pji_sim_len))
+            A[i, j] = ((1 - dropout_prob) ** Pij_sim_len * dropout_prob ** (Union_len - Pij_sim_len) * (
+                        1 - dropout_prob) ** Pji_sim_len * dropout_prob ** (Union_len - Pji_sim_len))
+            S1 = len(set([l for l in np.flatnonzero(X[i, :])]))
+            S2 = len(set([l for l in np.flatnonzero(X[j, :])]))
+            # B=(p_i)**(-Overlap_len)*(1-p_i)**(-barcodes+Overlap_len)*scipy.special.binom(barcodes,Union_len)/(scipy.special.binom(barcodes,S1)*scipy.special.binom(barcodes,S2))
+            p_double = 1 - ((p_i) ** 1 * (1 - p_i) ** (barcodes - 1) * scipy.special.binom(barcodes, 1) + (p_i) ** 0 * (
+                        1 - p_i) ** (barcodes - 0) * scipy.special.binom(barcodes, 0))
+            B = (p_double) ** (Overlap_len)
+            #  B=scipy.special.binom(barcodes,S1+S2)/(scipy.special.binom(S1+S2,S1)*scipy.special.binom(S1+S2,S2))
+            #  print(set([l for l in np.flatnonzero(X[i,:])]),set([l for l in np.flatnonzero(X[j,:])]),B,1-A[i,j], p_double)
+            A[i, j] = (1 - A[i, j]) * (B)
+            A[j, i] = A[i, j]
+            if j == i:
+                A[i, j] = 0
+
+        return A
+
+
+
+
+
+class Simulation:
+
+    def __init__(self, barcodes, cells, generations):
+        self.barcodes = barcodes
+        self.cells = cells
+        self.generations = generations
+        self.X = np.zeros([cells, barcodes, generations], dtype=int)
+
+    def propagation(self, system):
+        system_prop = system.copy()
+        for i in range(self.generations):
+            system_prop = np.concatenate((system_prop.copy(), system_prop.copy()))
+        return system_prop
+
+    def sampling(self, system):
+        return random.sample(list(system.copy()), system.shape[0] // (2 ** self.generations))
+
+    def run(self, system, sampling_times):
+        system_temp = system.copy()
+        for i in range(sampling_times):
+            system_temp_prop = self.propagation(system_temp.copy())
+            system_temp = self.sampling(system_temp_prop.copy())
+        return system_temp
+
+
+def barcodes_id_list(system):
+    labels = ["" for x in range(system.shape[0])]
+    for k in range(system.shape[0]):
+        labels[k] = ''.join("+ ").join([str(l) for l in np.flatnonzero(system[k, :])])
+    return labels
+
+
+def clustering(barcodes_in_cells_list, Threshold):
+    A_dist = LargeSystem.NumBarcodesDiff(np.array(barcodes_in_cells_list))
+
+    clustering = AgglomerativeClustering(distance_threshold=Threshold, n_clusters=None, affinity='precomputed',
+                                         linkage='complete',
+                                         compute_full_tree=True).fit(A_dist)
+    return clustering.labels_
+
 
---
+++ main.py
@@ -0,0 +1,274 @@
+
+from functions import *
+
+barcodes = 10 ** 4
+cells = 10 ** 3
+
+wrong_lin_split_hamming = np.zeros([20, 10, 2000])  # dimensions [p_inseration, p_dropouts, thershold ]
+wrong_lin_merge_hamming = np.zeros([20, 10, 2000])  # dimensions [p_inseration, p_dropouts, thershold ]
+
+wrong_lin_split_drop = np.zeros([20, 10, 2000])  # dimensions [p_inseration, p_dropouts, thershold ]
+wrong_lin_merge_drop = np.zeros([20, 10, 2000])  # dimensions [p_inseration, p_dropouts, thershold ]
+
+wrong_lin_split_drop_ins = np.zeros([20, 10, 2000])  # dimensions [p_inseration, p_dropouts, thershold ]
+wrong_lin_merge_drop_ins = np.zeros([20, 10, 2000])  # dimensions [p_inseration, p_dropouts, thershold ]
+
+number_true_clusters = np.zeros([20, 10, 2000])
+number_measured_clusters = np.zeros([20, 10, 2000])
+number_cluster_after_matching_Hamming = np.zeros([20, 10, 2000])
+number_cluster_after_matching_drop = np.zeros([20, 10, 2000])
+number_cluster_after_matching_drop_ins = np.zeros([20, 10, 2000])
+
+Num_of_measured_lin = np.zeros([20, 10, 2000])
+
+num_clusters = np.zeros([20, 10])
+
+score_split_Hamming = np.zeros([20, 10])
+score_merge_Hamming = np.zeros([20, 10])
+best_dist_Hamming = np.zeros([20, 10])
+score_split_drop = np.zeros([20, 10])
+score_merge_drop = np.zeros([20, 10])
+best_dist_drop = np.zeros([20, 10])
+score_split_drop_ins = np.zeros([20, 10])
+score_merge_drop_ins = np.zeros([20, 10])
+best_dist_drop_ins = np.zeros([20, 10])
+
+T = np.zeros([20, 10])
+
+epsilon = 10 ** -323
+
+data_full = {}
+
+ins = [0.5, 2, 10]
+for p_i in range(3):
+
+    # p_ins=p_i+0.5
+    p_ins = ins[p_i]
+
+    system_init = LargeSystem(barcodes=barcodes, cells=cells)
+    p = p_ins / barcodes
+    system_true = system_init.generate_barcoded_cells(p=p, Poisson=True)
+
+    system_true = system_true[system_true.sum(axis=1) > 0]  # taking only cells with labels (=barcodes inserted)
+
+    df = pd.DataFrame(barcodes_id_list(system_true), columns=['barcodes_id_true'])
+
+    data = {}
+
+    data['barcodes_id_true'] = barcodes_id_list(system_true)
+
+    sys = Simulation(barcodes=barcodes, cells=cells, generations=1)
+
+    gens = 30
+    system2 = sys.run(system_true, 5)
+    df['propgation' + str(gens) + '_true'] = barcodes_id_list(np.array(system2))
+
+    system4 = sys.run(system2, 5)
+    df['propgation' + str(2 * gens) + '_true'] = barcodes_id_list(np.array(system4))
+
+    system6 = sys.run(system4, 10)
+    df['propgation' + str(3 * gens) + '_true'] = barcodes_id_list(np.array(system6))
+
+    Total_true = np.concatenate((system2, system4, system6))
+    A_total_true = LargeSystem.Rapid_SimilarityMatrix(Total_true)
+
+    df_total = pd.DataFrame(barcodes_id_list(Total_true), columns=['true'])
+
+    clustering_total = AgglomerativeClustering(distance_threshold=epsilon, n_clusters=None, affinity='precomputed',
+                                               linkage='complete',
+                                               compute_full_tree=True).fit(A_total_true)
+
+    df_total['true_cluster'] = clustering_total.labels_
+
+    print('p_ins=', p_ins)
+    drop = [0, 0.01, 0.1, 0.5]
+    for p_d in range(4):
+
+        # p_drop=p_d/10
+        p_drop = drop[p_d]
+
+        system_true_drop = LargeSystem.dropouts(np.array(system_true).copy(), p=p_drop)
+
+        df['drop0'] = barcodes_id_list(np.array(system_true_drop))
+
+        # A_dist0=LargeSystem.SimilarityMatrix(system_true_drop)
+
+        system2_drop = LargeSystem.dropouts(np.array(system2).copy(), p=p_drop)
+
+        df['drop1'] = barcodes_id_list(np.array(system2_drop))
+
+        # A_dist1=LargeSystem.Rapid_SimilarityMatrix(system2_drop)
+
+        system4_drop = LargeSystem.dropouts(np.array(system4).copy(), p=p_drop)
+
+        df['drop2'] = barcodes_id_list(np.array(system4_drop))
+
+        # A_dist2=LargeSystem.Rapid_SimilarityMatrix(system4_drop)
+
+        system6_drop = LargeSystem.dropouts(np.array(system6).copy(), p=p_drop)
+
+        df['drop3'] = barcodes_id_list(np.array(system6_drop))
+
+        # A_dist3=LargeSystem.Rapid_SimilarityMatrix(system6_drop)
+
+        Total_drop = np.concatenate((system2_drop, system4_drop, system6_drop))
+
+        #######
+        A_total_drop_Hamming = LargeSystem.Rapid_SimilarityMatrix(Total_drop)
+        A_total_drop_drop = LargeSystem.DistanceMatrix_dropout(Total_drop, dropout_prob=p_drop)
+        A_total_drop_ins = LargeSystem.DistanceMatrix_dropout_conditional(Total_drop, dropout_prob=p_drop, p_i=p)
+        #######
+
+        A_total_drop_Hamming = A_total_drop_Hamming / (A_total_drop_Hamming.max())
+        A_total_drop_drop = A_total_drop_drop / (A_total_drop_drop.max())
+        A_total_drop_ins = A_total_drop_ins / (A_total_drop_ins.max())
+
+        df_total['dropped'] = barcodes_id_list(Total_drop)
+
+        A_total_drop_CompleteOverlap = LargeSystem.NumBarcodesDiff(Total_drop)
+        clustering_total = AgglomerativeClustering(distance_threshold=epsilon, n_clusters=None, affinity='precomputed',
+                                                   linkage='complete',
+                                                   compute_full_tree=True).fit(A_total_drop_CompleteOverlap)
+
+        df_total['dropped_cluster'] = clustering_total.labels_
+
+        Clustering_Threshold: int
+        for Clustering_Threshold in range(200):
+            Thres = Clustering_Threshold / 199 + epsilon
+
+            clustering_total_Hamming = AgglomerativeClustering(distance_threshold=Thres, n_clusters=None,
+                                                               affinity='precomputed', linkage='complete',
+                                                               compute_full_tree=True).fit(A_total_drop_Hamming)
+            clustering_total_drop = AgglomerativeClustering(distance_threshold=Thres, n_clusters=None,
+                                                            affinity='precomputed', linkage='complete',
+                                                            compute_full_tree=True).fit(A_total_drop_drop)
+            clustering_total_drop_ins = AgglomerativeClustering(distance_threshold=Thres, n_clusters=None,
+                                                                affinity='precomputed', linkage='complete',
+                                                                compute_full_tree=True).fit(A_total_drop_ins)
+
+            # temp= clustering_total.labels_
+            # print([temp[x:x+int(len(data['barcodes_id_true']) )] for x in range(0, len(temp)  , int(len(data['barcodes_id_true']) ))])
+            # temp1=[temp[x:x+int(len(data['barcodes_id_true']) )] for x in range(0, len(temp)  , int(len(data['barcodes_id_true']) ))]
+            # df['cluster1']=temp1[0]
+            # df['cluster2']=temp1[1]
+            # df['cluster3']=temp1[2]
+
+            df_total['matching_after_drop_cluster_Hamming'] = clustering_total_Hamming.labels_
+            df_total['matching_after_drop_cluster_drop'] = clustering_total_drop.labels_
+            df_total['matching_after_drop_cluster_drop_ins'] = clustering_total_drop_ins.labels_
+
+            cm_pred_hamming = confusion_matrix(df_total[df_total['dropped'] != ''].true_cluster,
+                                               df_total[df_total['dropped'] != ''].matching_after_drop_cluster_Hamming)
+            cm_pred_drop = confusion_matrix(df_total[df_total['dropped'] != ''].true_cluster,
+                                            df_total[df_total['dropped'] != ''].matching_after_drop_cluster_drop)
+            cm_pred_drop_ins = confusion_matrix(df_total[df_total['dropped'] != ''].true_cluster, df_total[
+                df_total['dropped'] != ''].matching_after_drop_cluster_drop_ins)
+
+            wrong_lin_split_hamming[p_i, p_d, Clustering_Threshold] = (np.amax(cm_pred_hamming, axis=1).sum(
+                axis=0)).sum() / (df_total[df_total['dropped']!=''].shape[0])
+            wrong_lin_merge_hamming[p_i, p_d, Clustering_Threshold] = (np.amax(cm_pred_hamming, axis=0).sum(
+                axis=0)).sum() / (df_total[df_total['dropped']!=''].shape[0])
+
+            wrong_lin_split_drop[p_i, p_d, Clustering_Threshold] = (np.amax(cm_pred_drop, axis=1).sum(axis=0)).sum() / (
+                        df_total[df_total['dropped']!=''].shape[0])
+            wrong_lin_merge_drop[p_i, p_d, Clustering_Threshold] = (np.amax(cm_pred_drop, axis=0).sum(axis=0)).sum() / (
+                        df_total[df_total['dropped']!=''].shape[0])
+
+            wrong_lin_split_drop_ins[p_i, p_d, Clustering_Threshold] = (np.amax(cm_pred_drop_ins, axis=1).sum(
+                axis=0)).sum() / (df_total[df_total['dropped']!=''].shape[0])
+            wrong_lin_merge_drop_ins[p_i, p_d, Clustering_Threshold] = (np.amax(cm_pred_drop_ins, axis=0).sum(
+                axis=0)).sum() / (df_total[df_total['dropped']!=''].shape[0])
+
+            number_true_clusters[p_i, p_d, Clustering_Threshold] = df_total[
+                df_total['dropped'] != ''].true_cluster.nunique()
+            number_measured_clusters[p_i, p_d, Clustering_Threshold] = df_total[
+                df_total['dropped'] != ''].dropped_cluster.nunique()
+
+            number_cluster_after_matching_Hamming[p_i, p_d, Clustering_Threshold] = df_total[
+                df_total['dropped'] != ''].matching_after_drop_cluster_Hamming.nunique()
+            number_cluster_after_matching_drop[p_i, p_d, Clustering_Threshold] = df_total[
+                df_total['dropped'] != ''].matching_after_drop_cluster_drop.nunique()
+            number_cluster_after_matching_drop_ins[p_i, p_d, Clustering_Threshold] = df_total[
+                df_total['dropped'] != ''].matching_after_drop_cluster_drop_ins.nunique()
+
+        #  Num_of_measured_lin[p_i,p_d,Clustering_Threshold]=number_cluster_after_matching[p_i,p_d,Clustering_Threshold]/number_true_clusters[p_i,p_d,Clustering_Threshold]-1
+
+        R = list(abs(1 - number_cluster_after_matching_Hamming[p_i, p_d, :] / number_true_clusters[p_i, p_d, 0]))
+        best_i = R.index(min(R))
+        score_split_Hamming[p_i, p_d] = wrong_lin_split_hamming[p_i, p_d, best_i]
+        score_merge_Hamming[p_i, p_d] = wrong_lin_merge_hamming[p_i, p_d, best_i]
+        best_dist_Hamming[p_i, p_d] = best_i / 199 + epsilon
+
+        R = list(abs(1 - number_cluster_after_matching_drop[p_i, p_d, :] / number_true_clusters[p_i, p_d, 0]))
+        best_i = R.index(min(R))
+        score_split_drop[p_i, p_d] = wrong_lin_split_drop[p_i, p_d, best_i]
+        score_merge_drop[p_i, p_d] = wrong_lin_merge_drop[p_i, p_d, best_i]
+        best_dist_drop[p_i, p_d] = best_i / 199 + epsilon
+
+        R = list(abs(1 - number_cluster_after_matching_drop_ins[p_i, p_d, :] / number_true_clusters[p_i, p_d, 0]))
+        best_i = R.index(min(R))
+        score_split_drop_ins[p_i, p_d] = wrong_lin_split_drop_ins[p_i, p_d, best_i]
+        score_merge_drop_ins[p_i, p_d] = wrong_lin_merge_drop_ins[p_i, p_d, best_i]
+        best_dist_drop_ins[p_i, p_d] = best_i / 199 + epsilon
+
+        print(p_i,p_d,score_split_Hamming[p_i,p_d])
+
+#print(score_split_Hamming[:3,:4])
+cmap = plt.get_cmap('viridis', 20)
+plt.figure(figsize=(25,10))
+plt.subplot(3, 3, 1)
+plt.pcolormesh(score_split_Hamming[0:3,0:4].T,vmin=0,vmax=1,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('dropout prob.')
+plt.title('best score split')
+plt.colorbar()
+plt.subplot(3, 3, 2)
+plt.pcolormesh(score_merge_Hamming[:3,:4].T,vmin=0,vmax=1,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.title('best score merge')
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('dropout prob.')
+plt.colorbar()
+plt.subplot(3, 3, 3)
+plt.pcolormesh(best_dist_Hamming[:3,:4].T,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.title('best dist = matching threshold')
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('dropout prob.')
+plt.colorbar()
+
+plt.subplot(3, 3, 4)
+plt.pcolormesh(score_split_drop[:3,:4].T,vmin=0,vmax=1,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('dropout prob.')
+plt.title('best score split')
+plt.colorbar()
+plt.subplot(3, 3, 5)
+plt.pcolormesh(score_merge_drop[:3,:4].T,vmin=0,vmax=1,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.title('best score merge')
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('droupout prob.')
+plt.colorbar()
+plt.subplot(3, 3, 6)
+plt.pcolormesh(best_dist_drop[:3,:4].T,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.title('best dist = matching thershold')
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('droupout prob.')
+plt.colorbar()
+plt.subplot(3, 3, 7)
+plt.pcolormesh(score_split_drop_ins[:3,:4].T,vmin=0,vmax=1,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('droupout prob.')
+plt.title('best scrore split')
+plt.colorbar()
+plt.subplot(3, 3, 8)
+plt.pcolormesh(score_merge_drop_ins[:3,:4].T,vmin=0,vmax=1,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.title('best scrore merge')
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('droupout prob.')
+plt.colorbar()
+plt.subplot(3, 3, 9)
+plt.pcolormesh(best_dist_drop_ins[:3,:4].T,cmap=cmap,edgecolors='k', linewidths=0.1)
+plt.title('best dist = matching thershold')
+plt.xlabel('mean barcodes cassette')
+plt.ylabel('droupout prob.')
+plt.colorbar()
+plt.show()
